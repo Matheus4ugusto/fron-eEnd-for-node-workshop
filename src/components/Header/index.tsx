f9d14c4526b5930d381a60db028a979e8f217fb2
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2023-12-13 13:25:52 -0300

    Implementa os redirects a partir do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,37 @@ import * as Styled from "./header.style";
 import {FaNewspaper, FaRegUserCircle} from "react-icons/fa";
 import {IoIosMore} from "react-icons/io";
 import {BiHelpCircle} from "react-icons/bi";
-import {redirect} from "next/navigation";
+import {redirect, useRouter} from "next/navigation";
 
 const Header: React.FC = () => {
+
+    const router = useRouter();
+    const handleSignUp = (e: any) => {
+        e.preventDefault()
+        router.push("/register")
+    }
+    const handleSignIn = (e: any) => {
+        e.preventDefault()
+        router.push("/login")
+    }
+    const handleHelp = (e: any) => {
+        e.preventDefault()
+        router.push("/help")
+    }
+    const handleHome = (e: any) => {
+        e.preventDefault()
+        router.push("/")
+    }
 
     const [popUp, setPopUP] = useState(false);
     return (
         <Styled.Header>
-            <Styled.Button name="O Blog" aria-label="O Blog">
+            <Styled.Button name="O Blog" aria-label="O Blog" onClick={handleHome}>
                 <FaNewspaper/>
                 O Blog
             </Styled.Button>
             <Styled.Div>
-                <Styled.Button id="sign-in-button" name="Entrar" aria-label="Entrar">
+                <Styled.Button id="sign-in-button" name="Entrar" aria-label="Entrar" onClick={handleSignIn}>
                     Entrar
                 </Styled.Button>
                 <Styled.Button
@@ -29,11 +47,12 @@ const Header: React.FC = () => {
             </Styled.Div>
             {popUp && (
                 <Styled.Section>
-                    <Styled.PopUpButton id="sign-up-button" name="Cadastre-se" aria-label="Cadastre-se">
+                    <Styled.PopUpButton id="sign-up-button" name="Cadastre-se" aria-label="Cadastre-se"
+                                        onClick={handleSignUp}>
                         <FaRegUserCircle/>
                         Cadastre-se
                     </Styled.PopUpButton>
-                    <Styled.PopUpButton name="Precisa de ajuda?" aria-label="Precisa de ajuda?">
+                    <Styled.PopUpButton name="Precisa de ajuda?" aria-label="Precisa de ajuda?" onClick={handleHelp}>
                         <BiHelpCircle/>
                         Precisa de ajuda?
                     </Styled.PopUpButton>
